2037d8ce12e2560032938eaa3e9e237e21f9744e
Ellena Tzavelas <wakandacat@gmail.com>
2024-08-22 21:32:35 -0400

    cycling images for each project

--- src/pageComponents/AboutPage.jsx
+++ src/pageComponents/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../styles/Cards.css';
-import '../styles/AboutPage.css';
+import '../styles/ProjectPages.css';
 
 function AboutPage() {
 
--- src/pageComponents/AcademicPage.jsx
+++ src/pageComponents/AcademicPage.jsx
@@ -6,12 +6,16 @@ function AcademicPage() {
 
     const [jsonData, setJsonData] = useState(null);
     const [projArr, setProjArr] = useState([]); // Store the rendered JSX elements
+    const [currProjImage, setCurrProjImage] = useState(0);
 
     //top page elements
     const projTitle = useRef();
     const projYear= useRef();
     const projBlurb = useRef();
     const projImage = useRef();
+    const projAlt = useRef();
+    const projImageArr = useRef();
+    const projImageArr2 = useRef();
 
     var jsonFile = '/projectInfo.json';
 
@@ -41,7 +45,7 @@ function AcademicPage() {
                 <button className="proj-button" key={index} value={JSON.stringify(element)} onClick={() => handleClick(element, event)}>
                     <div className="project">
                         <h2 className="proj-title">{element.Title.toUpperCase()}</h2>
-                        <img className="proj-image" src={`src/assets/${element.Image}`}/>
+                        <img className="proj-image" src={`src/assets/${element.Image[0].img}`}/>
                     </div>
                 </button>
             ));
@@ -59,12 +63,48 @@ function AcademicPage() {
         //force page to top
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        //add the arrow buttons and alt text
+        projImageArr.current.style.visibility = "visible";
+        projImageArr2.current.style.visibility = "visible";
+        projAlt.current.style.visibility = "visible";
+
         //update the top page card with the project info
         projTitle.current.textContent = element.Title.toUpperCase();
         projYear.current.textContent = element.Year;
         projBlurb.current.textContent = element.Desc;
-        projImage.current.src = `src/assets/${element.Image}`;
+        projImage.current.src = `src/assets/${element.Image[0].img}`;
+        projAlt.current.textContent = element.Image[0].alt;
+        projImageArr.current.value = JSON.stringify(element.Image);
+        projImageArr2.current.value = JSON.stringify(element.Image);
+        setCurrProjImage(0);
     }
+
+    //change project view on button click with cycling
+    const handleCycle = (num,event) => {
+        let ImageArr = JSON.parse(projImageArr.current.value);
+        let newIndex = 0;
+        if(num === 0){
+            newIndex = currProjImage - 1;
+            if(newIndex < 0){
+                newIndex = ImageArr.length - 1;
+            }
+        } else {
+            newIndex = currProjImage + 1;
+            newIndex = newIndex%ImageArr.length;
+        }
+        
+        setCurrProjImage(newIndex);
+    }
+
+    //update the project image and alt text when arrow buttons clicked
+    useEffect(() => {
+        if (jsonData !== null) {
+            let ImageArr = JSON.parse(projImageArr.current.value);
+            //update the image
+            projImage.current.src = `src/assets/${ImageArr[currProjImage].img}`;
+            projAlt.current.textContent = ImageArr[currProjImage].alt;
+        }
+    }, [currProjImage]);
 
     return (
         <>
@@ -74,8 +114,16 @@ function AcademicPage() {
                     <h2 ref={projYear} className="sub-title">Carleton University // Algonquin College</h2>
                     <h3 ref={projBlurb} className="blurb">these are my projects from school yall</h3>
                 </div>
-                <img ref={projImage} src='src\assets\cat.jpg'/>
+                <div className="proj-image-container">
+                    <div id="cycle-image">
+                        <button className="arrow-button" ref={projImageArr} value={0} onClick={() => handleCycle(0,event)}><p>&#9664;</p></button>
+                        <img id="projIMG" ref={projImage} src='src\assets\cat.jpg'/>
+                        <button className="arrow-button" ref={projImageArr2} value={0} onClick={() => handleCycle(1,event)}><p>&#9658;</p></button>
+                    </div>    
+                    <h5 ref={projAlt} id="alt-text">hey guys this is a cat from google images</h5>
+                </div>
             </div>
+            <div id="tri-design"></div>
             <div className="page-bottom card">
                 <div className="flex-container main-container">
                 {projArr}
